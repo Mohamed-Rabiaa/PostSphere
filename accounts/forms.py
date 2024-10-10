1a6b60d44dcc9d38e54355a747eecf59f2be7aa4
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-10-10 18:59:49 +0300

    Add the forms & views of the account app

---
+++ accounts/forms.py
@@ -0,0 +1,53 @@
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+from django import forms
+from .models import User
+
+class RegisterForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=50)
+    email = forms.EmailField(label='Email', max_length=50)
+    password = forms.CharField(label='Password', max_length=50,
+                               widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='Confirm Password', max_length=50,
+                                       widget=forms.PasswordInput)
+
+    # Custom validation for the password match
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError('Passwords do not match.')
+        return cleaned_data
+
+    # Unique email validation
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with this email already exists.")
+        return email
+
+    # Password validation
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        validate_password(password)
+        return password
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email', max_length=50)
+    password = forms.CharField(label='Password', max_length=50,
+                               widget=forms.PasswordInput)
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')        
+        #Check if the email exists in the database
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise ValidationError("Invalid Email.")
+
+        password = cleaned_data.get('password')
+        #Check if the given password belongs to the user with the given email
+        if not user.check_password(password):
+            raise ValidationError("Invalid Password")
+        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login as auth_login
+from .forms import RegisterForm, LoginForm
+from django.contrib import messages
+from .models import User
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            #Creating the new user
+            user = User.objects.create_user(name, email, password)
+            #Logging in the user
+            auth_login(request, user)
+            
+            print('The user with {} logged in'.format(email))
+            return render(request, 'blog/home.html')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                auth_login(request, user)
+                print('The user with {} logged in'.format(email))
+                return render(request, 'blog/home.html')
+            else:
+                form.add_error(None, "Invalid login credentials")
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
